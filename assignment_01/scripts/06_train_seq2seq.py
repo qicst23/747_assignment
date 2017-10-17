17aed7dfa4bdca88771c494749dad0e508f2804f
Sai Krishna <srallaba@cs.cmu.edu>
2017-10-17 15:16:24 -0400

    Added Sequence to Sequence model

---
+++ assignment_01/scripts/06_train_seq2seq.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+import numpy as np
+import dynet as dy
+from dynet_modules import sequence_to_sequence
+from utils import Vocab
+from collections import Counter, defaultdict
+from itertools import count
+import random
+
+src_file = '../data/sequence_pairs_src.txt'
+tgt_file = '../data/sequence_pairs_tgt.txt'
+num_hidden = 128
+num_input = 128
+num_layers = 1
+num_epochs = 50
+early_save = 1
+
+def read(fname):
+    """
+    Read a file where each line is of the form "word1 word2 ..."
+    Yields lists of the form [word1, word2, ...]
+    """
+    with file(fname) as fh:
+        for line in fh:
+            sent = line.strip().split()
+            sent.append("<s>")
+            yield sent
+
+# Read the file and extract all sequences
+src = list(read(src_file))
+tgt = list(read(tgt_file))
+all_sequences = zip(src,tgt)
+
+# Train Valid split
+train = []
+valid = []
+for a, k in enumerate(all_sequences):
+   if a%10 == 1:
+      valid.append(k)
+   else:
+      train.append(k) 
+
+
+# Word Count and representation
+words=[]
+wc=Counter()
+for sent in src:
+    for w in sent:
+        words.append(w)
+        wc[w]+=1
+
+for sent in tgt:
+    for w in sent:
+        words.append(w)
+        wc[w]+=1
+
+vw = Vocab.from_corpus([words])
+S = vw.w2i["<s>"]
+nwords = vw.size()
+
+
+model = dy.Model()
+trainer = dy.AdamTrainer(model)
+seq2seq = sequence_to_sequence(num_layers, num_input, num_hidden, model, nwords, vw)
+
+
+# Have fun
+
+num_tagged = cum_loss = 0
+for ITER in xrange(50):
+    random.shuffle(train)
+    for i,s in enumerate(train,1):
+        if i % 1000 == 0:
+            trainer.status()
+            print cum_loss / num_tagged , " processed ", i , " of ", len(train)
+            cum_loss = 0
+            num_tagged = 0
+        if i % 10000 == 0 or i == len(train)-1:
+            dev_loss = dev_words = 0
+            for sent in valid:
+                loss_exp = seq2seq.calculate_sequence_loss(sent[0], sent[1])
+                dev_loss += loss_exp.scalar_value()
+                dev_words += len(sent[0])
+            print dev_loss / dev_words
+            # Save the model
+            seq2seq.save('models_seq2seq')
+            print "Saved model"
+        # train on sent
+        loss_exp = seq2seq.calculate_sequence_loss(s[0], s[1])
+        cum_loss += loss_exp.scalar_value()
+        num_tagged += len(s[0])
+        loss_exp.backward()
+        trainer.update()
+        if early_save == 1:
+           seq2seq.save('models_seq2seq')
+           print "Saved after first example"
+           early_save =0 
+    print "epoch %r finished" % ITER
+    trainer.update_epoch(1.0)
+
+    # Save the model
+    seq2seq.save('models')
+    print "Saved model"
+ 
+
+
+
--- assignment_01/scripts/dynet_modules.py
+++ assignment_01/scripts/dynet_modules.py
@@ -112,6 +112,110 @@ class language_model(SaveableModel):
         s = s.add_input(self.lookup[wid]) 
       return dy.esum(losses)
   
+class sequence_to_sequence(SaveableModel):
+   
+   def __init__(self, num_layers, num_input, num_hidden, model, nwords, vw):
+     self.num_layers = num_layers
+     self.num_input = num_input
+     self.num_hidden = num_hidden
+     self.model = model
+     self.current_RNN = dy.LSTMBuilder(self.num_layers, self.num_input, self.num_hidden, self.model)
+     self.next_RNN = dy.LSTMBuilder(self.num_layers, self.num_input, self.num_hidden, self.model)
+     self.nwords = nwords
+     self.vw = vw
+     self.add_params()
+
+   def add_params(self):
+     ### This is to add extra parameters to put the model on steroids
+     
+     # Softmax weights/biases on top of LSTM outputs
+     self.W_sm = self.model.add_parameters((self.nwords, self.num_hidden))
+     self.b_sm = self.model.add_parameters(self.nwords)
+     self.lookup = self.model.add_lookup_parameters((self.nwords, self.num_input))
+    
+     
+   def get_LM(self):
+     return self.RNN
+   
+   def get_model(self):
+     return self.model
+
+   def save(self, path):
+     if not os.path.exists(path): os.makedirs(path)
+     arr = [ self.num_layers, self.num_input, self.num_hidden, self.nwords, self.vw ]
+     self.model.save(path + '/model')
+     with open(path + '/model_hyps', 'w') as f: pickle.dump(arr, f) 
+
+   def print_params(self):
+      print self.num_layers
+      print self.num_input
+      print self.num_hidden
+
+   def load_model(self, model, path):
+      model.populate(path + '/model')
+      with open(path+"/model_hyps", "r") as f: arr = pickle.load(f)
+      self.model = model
+      self.num_layers = arr[0]
+      self.num_input = arr[1]
+      self.num_hidden = arr[2]
+      self.nwords = arr[3]
+      self.vw= arr[4]
+
+   def load_copy(self, model, path):
+       if not os.path.exists(path): raise Exception("Model "+path+" does not exist")
+       with open(path+"/model_hyps", "r") as f: arr = pickle.load(f)
+       model.populate(path + '/model')
+       return model
+
+   @staticmethod
+   def load(model, path):
+       if not os.path.exists(path): raise Exception("Model "+path+" does not exist")
+       with open(path+"/model_hyps", "r") as f: arr = pickle.load(f)
+       model.populate(path + '/model')
+       return model
+   
+   def calculate_sequence_loss(self, current_sequence, next_sequence):
+      
+      # Renew the computation graph
+      dy.renew_cg()
+ 
+      # Initialize the current RNN
+      f_init = self.current_RNN.initial_state()
+  
+      # Initialize the parameters
+      W_exp = dy.parameter(self.W_sm)
+      b_exp = dy.parameter(self.b_sm)
+
+      # Get the ids for ICD codes
+      wids_current = [self.vw.w2i[w] for w in current_sequence]
+
+      # Start the RNN
+      s = f_init.add_input(dy.lookup(self.lookup, wids_current[-1])) 
+
+      # Feed the vectors into the current RNN and predict the next code
+      losses = []
+      for wid in wids_current:
+        #score = W_exp * s.output() + b_exp
+        #loss = dy.pickneglogsoftmax(score, wid)
+        #losses.append(loss)
+        s = s.add_input(self.lookup[wid])
+
+      
+      #self.next_RNN.initial_state().add_input(s)
+      # Initialize the next RNN
+      s_next = self.next_RNN.initial_state().add_input(s.output())
+      
+      # Get the ids for ICD codes
+      wids_next = [self.vw.w2i[w] for w in next_sequence]
+
+      # Feed the vectors into the next RNN and predict the next codes
+      for wid in wids_next:
+        score = W_exp * s_next.output() + b_exp
+        loss = dy.pickneglogsoftmax(score, wid)
+        losses.append(loss)
+        s = s.add_input(self.lookup[wid])  
+      return dy.esum(losses)
+  
 
 class Vocab:
 
